509bad31b4eb00cd29ced5b674754387479561ba
MattMofDoom <matt.marlor@outlook.com>
2021-06-03 16:37:24 +1000

    Add something to LurgleTest .. no config yet

--- LurgleTest/Program.cs
+++ LurgleTest/Program.cs
@@ -1,38 +1,12 @@
-﻿
-/* Unmerged change from project 'LurgleTest (net48)'
-Before:
-using System;
-After:
-using FluentEmail.Core;
-*/
+﻿using Lurgle.Alerting;
 
-/* Unmerged change from project 'LurgleTest (net47)'
-Before:
-using System;
-After:
-using FluentEmail.Core;
-*/
-
-/* Unmerged change from project 'LurgleTest (net48)'
-Before:
-using FluentEmail.Core;
-After:
-using System;
-*/
-
-/* Unmerged change from project 'LurgleTest (net47)'
-Before:
-using FluentEmail.Core;
-After:
-using System;
-*/
 namespace LurgleTest
 {
     internal class Program
     {
         private static void Main()
         {
-
+            Alert.From("Bob@Builder.com").Subject("Test").Send("Can you fix it?");
         }
     }
 }
