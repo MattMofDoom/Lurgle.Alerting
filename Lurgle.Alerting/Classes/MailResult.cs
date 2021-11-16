85ce2bca378cf7e8154e924a2e434d045550805d
MattMofDoom <matt.marlor@outlook.com>
2021-11-16 14:15:04 +1100

    Fix bugs with sending attachments, alternate text templates

--- Lurgle.Alerting/Classes/MailResult.cs
+++ Lurgle.Alerting/Classes/MailResult.cs
@@ -8,37 +8,37 @@
 namespace Lurgle.Alerting.Classes
 {
     /// <summary>
-    /// Record of mail send result, including all results for iteration
+    ///     Record of mail send result, including all results for iteration
     /// </summary>
     public class MailResult
     {
         /// <summary>
-        /// Overall Success / Failure
+        ///     Overall Success / Failure
         /// </summary>
         public bool Successful { get; set; }
 
         /// <summary>
-        /// Last send's delivery type
+        ///     Last send's delivery type
         /// </summary>
         public DeliveryType DeliveryType { get; set; }
 
         /// <summary>
-        /// Last send's mail host
+        ///     Last send's mail host
         /// </summary>
         public string MailHost { get; set; }
 
         /// <summary>
-        /// Last send's ErrorMessages for backward compatibility
+        ///     Last send's ErrorMessages for backward compatibility
         /// </summary>
         public IList<string> ErrorMessages { get; set; }
 
         /// <summary>
-        /// Last send's MessageId for backward compatibility
+        ///     Last send's MessageId for backward compatibility
         /// </summary>
         public string MessageId { get; set; }
 
         /// <summary>
-        /// List of all attempts
+        ///     List of all attempts
         /// </summary>
         public List<DeliveryAttempt> DeliveryAttempts { get; set; }
     }
